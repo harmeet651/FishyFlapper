2b321980de818b2cbc08c2a71e8d0071cbec645b
harmeet651 <harmeet651@gmail.com>
2018-10-06 01:54:37 -0700

    initial code

---
+++ Assets/Art/Scripts/fishController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishController : MonoBehaviour {
+
+    [SerializeField]
+    private float jumpForce = 50f;
+    private float GoRight = 10f;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.right * GoRight);
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
---
+++ Assets/Art/Scripts/obstacleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
